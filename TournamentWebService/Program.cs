925b2611316b741b348e60223b0c523ba76eeef4
Daniel <dgalindoca@unal.edu.co>
2023-05-08 13:38:37 -0500

    Fixed for docker container creation

--- TournamentWebService/Program.cs
+++ TournamentWebService/Program.cs
@@ -7,6 +7,30 @@ using TournamentWebService.Tournaments.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+
+// Set port (for container running)
+builder.WebHost.UseUrls("http://*:443");
+
+
+// Find connectionURI in environment variables (doesn't work)
+//TournamentsMongoDBSettings tournamentsMongoDBSettings = builder.Configuration.GetSection("TournamentsMongoDB").Get<TournamentsMongoDBSettings>();
+//string tournamentConnectionURI = String.IsNullOrEmpty(Environment.GetEnvironmentVariable("ASPNETCORE_TournamentsMongoDB__ConnectionURI"))
+//    ? "No ConnectionURI" : Environment.GetEnvironmentVariable("ASPNETCORE_TournamentsMongoDB__ConnectionURI");
+//tournamentsMongoDBSettings.ConnectionURI = tournamentConnectionURI;
+
+//Console.WriteLine("##################### LOGS #####################");
+//Console.WriteLine("tournamentConnectionURI: ", tournamentConnectionURI);
+//Console.WriteLine("tournamentsMongoDBSettings.ConnectionURI", tournamentsMongoDBSettings.ConnectionURI);
+
+
+// Configure Kestrel (ports for local debug)
+//builder.WebHost.UseKestrel(options =>
+//{
+//    options.ListenAnyIP(443); // to listen for incoming http connection on port 443
+//    options.ListenAnyIP(7001, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
+//});
+
 builder.Services.Configure<TournamentsMongoDBSettings>(builder.Configuration.GetSection("TournamentsMongoDB"));
 builder.Services.AddSingleton<TournamentMongoDBService>();
 builder.Services.Configure<TeamsMongoDBSettings>(builder.Configuration.GetSection("TeamsMongoDB"));
